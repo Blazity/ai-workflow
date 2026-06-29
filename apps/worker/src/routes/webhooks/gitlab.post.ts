48429669a23ed1eae31418e33b0700ce1bd34d17
Karol Chudzik <iipandarx@protonmail.com>
2026-06-29 20:26:18 +0200

    Add GitLab merge request webhook route

---
+++ apps/worker/src/routes/webhooks/gitlab.post.ts
@@ -0,0 +1,48 @@
+import { createError, defineEventHandler, getHeader, readRawBody } from "h3";
+import { env } from "../../../env.js";
+import {
+  normalizeGitLabMergeRequestEvent,
+  projectMatchesConfiguredId,
+  verifyGitLabWebhookToken,
+} from "../../lib/gitlab-webhook.js";
+import { logger } from "../../lib/logger.js";
+import { dispatchPostPrGateWebhook } from "../../lib/post-pr-gate-dispatch.js";
+
+const ALLOWED_ACTIONS = new Set(["opened", "update", "reopened"]);
+
+export default defineEventHandler(async (event) => {
+  const rawBody = (await readRawBody(event, "utf8")) ?? "";
+
+  try {
+    verifyGitLabWebhookToken(getHeader(event, "x-gitlab-token"), env.GITLAB_WEBHOOK_SECRET!);
+  } catch (err) {
+    throw createError({ statusCode: 401, statusMessage: (err as Error).message });
+  }
+
+  const gitLabEvent = getHeader(event, "x-gitlab-event");
+  if (gitLabEvent !== "Merge Request Hook") {
+    return { status: "ignored", reason: "not_merge_request_event" };
+  }
+
+  const body = rawBody ? JSON.parse(rawBody) : {};
+  if (!projectMatchesConfiguredId(body?.project, env.GITLAB_PROJECT_ID!)) {
+    logger.info(
+      { project: body?.project, expected: env.GITLAB_PROJECT_ID },
+      "post_pr_gate_gitlab_webhook_skipped_other_project",
+    );
+    return { status: "ignored", reason: "other_project" };
+  }
+
+  let normalized;
+  try {
+    normalized = normalizeGitLabMergeRequestEvent(body);
+  } catch {
+    return { status: "ignored", reason: "malformed_payload" };
+  }
+
+  if (!ALLOWED_ACTIONS.has(normalized.action)) {
+    return { status: "ignored", reason: `action_${normalized.action}` };
+  }
+
+  return dispatchPostPrGateWebhook(normalized);
+});
